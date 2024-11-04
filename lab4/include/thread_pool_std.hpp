13d0fe98831625791e4cb45b2a0a02ced73cf974
hsunekichi <hsunekichi@gmail.com>
2024-11-04 12:08:41 +0100

    Added std future p4

---
+++ lab4/include/thread_pool_std.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <functional>
+#include <vector>
+#include <thread>
+#include <future>
+
+class thread_pool_std
+{
+    std::vector<std::future<void>> futures;
+public:
+    // Constructor initializes the pool with a specified number of threads (default is hardware concurrency)
+    thread_pool_std() {}
+
+    // Destructor calls wait and stops the threads
+    ~thread_pool_std()
+    {
+        wait();       // Ensure all tasks are completed
+    }
+
+    // Wait for all tasks to complete (active waiting)
+    void wait()
+    {
+        for (auto& f : futures)
+        {
+            f.get();
+        }
+
+        futures.clear();
+    }
+
+    // Submit a new task to the pool
+    template<typename F>
+    void submit(F f)
+    {
+        futures.push_back(std::async(std::launch::async, f));
+    }
+};
